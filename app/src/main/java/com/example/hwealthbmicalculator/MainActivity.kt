38b80870e463a38ef45ba50c1f3e93eefb698470
yongzeng99 <57099518+yongzeng99@users.noreply.github.com>
2020-01-06 11:19:40 +0800

    HWEALTH

---
+++ app/src/main/java/com/example/hwealthbmicalculator/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.hwealthbmicalculator
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var heightInput: EditText
+    lateinit var weightInput: EditText
+    lateinit var resultImage: ImageView
+    lateinit var setResult: TextView
+    lateinit var setTextResult: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        resultImage = findViewById(R.id.imageViewProfile)
+        heightInput= findViewById(R.id.editTextHeight)
+        weightInput = findViewById(R.id.editTextWeight)
+        setResult = findViewById(R.id.textViewBMI)
+        setTextResult = findViewById(R.id.textResult)
+
+        val calculateButton : Button = findViewById(R.id.buttonCalculate)
+        val resetButton : Button = findViewById(R.id.buttonReset)
+
+        calculateButton.setOnClickListener{ calculateBMI() }
+        resetButton.setOnClickListener { resetBMI() }
+    }
+
+    private fun calculateBMI(){
+        //convert cm to meters
+        val heightDouble: Double = heightInput.text.toString().toDouble() / 100
+        val weightDouble: Double = weightInput.text.toString().toDouble()
+        val result: Double =  weightDouble / Math.pow(heightDouble, 2.0)
+
+
+
+        if (result < 18.5){
+            setTextResult.setTextColor(Color.parseColor("#2b4bff"))
+            setResult.setText("Your Category :" + String.format("%s", setTextResult.setText("Underweight")))
+        } else if (result >= 18.5 && result <= 24.9) {
+            setTextResult.setTextColor(Color.parseColor("##40ff49"));
+            setResult.setText("Your Category :" + setTextResult.setText("Normal"))
+        } else if (result >= 25 && result <= 29.9){
+            setTextResult.setTextColor(Color.parseColor("#ffa217"));
+            setResult.setText("Your Category :" + setTextResult.setText("Overweight"))
+        } else if (result >= 30){
+            setTextResult.setTextColor(Color.parseColor("##ff2626"));
+            setResult.setText("Your Category :" + setTextResult.setText("Obese"))
+        }
+
+
+    }
+
+    private fun resetBMI(){
+
+        setResult.setText("Your Category : ")
+        setTextResult.setText("")
+        heightInput.setText("")
+        weightInput.setText("")
+
+    }
+}
