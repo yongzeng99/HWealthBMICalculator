329b4fd419bd281a3e9c6e364cbe7187e81344de
yongzeng99 <57099518+yongzeng99@users.noreply.github.com>
2020-01-06 23:18:49 +0800

    HWEALTH

--- app/src/main/java/com/example/hwealthbmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/hwealthbmicalculator/MainActivity.kt
@@ -7,6 +7,25 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+import androidx.core.view.ViewCompat.setRotation
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import com.example.hwealthbmicalculator.databinding.ActivityMainBinding
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import androidx.databinding.DataBindingUtil
+import android.R.attr.pivotY
+import android.R.attr.pivotX
+import android.R.attr.angle
+import android.graphics.Matrix
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,20 +33,21 @@ class MainActivity : AppCompatActivity() {
     lateinit var weightInput: EditText
     lateinit var resultImage: ImageView
     lateinit var setTextResult: TextView
+    lateinit var setBmiResult: TextView
+    var binding: ActivityMainBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         resultImage = findViewById(R.id.imageViewProfile)
         heightInput= findViewById(R.id.editTextHeight)
         weightInput = findViewById(R.id.editTextWeight)
         setTextResult = findViewById(R.id.textResult)
+        setBmiResult = findViewById(R.id.bmiResult)
 
         val calculateButton : Button = findViewById(R.id.buttonCalculate)
-        val resetButton : Button = findViewById(R.id.buttonReset)
 
         calculateButton.setOnClickListener{ calculateBMI() }
-        resetButton.setOnClickListener { resetBMI() }
     }
 
     private fun calculateBMI(){
@@ -37,30 +57,28 @@ class MainActivity : AppCompatActivity() {
         val result: Double =  weightDouble / Math.pow(heightDouble, 2.0)
 
 
-
         if (result < 18.5){
             setTextResult.setTextColor(Color.parseColor("#2b4bff"))
             setTextResult.setText("Underweight")
+            indicator.animate().rotation(15f).start()
+            indicator.pivotX = 150f
         } else if (result >= 18.5 && result <= 24.9) {
             setTextResult.setTextColor(Color.parseColor("#40ff49"))
             setTextResult.setText("Normal")
+            indicator.animate().rotation(65f).start()
+            indicator.pivotX = 150f
         } else if (result >= 25 && result <= 29.9){
             setTextResult.setTextColor(Color.parseColor("#ffa217"))
             setTextResult.setText("Overweight")
+            indicator.animate().rotation(115f).start()
+            indicator.pivotX = 150f
         } else if (result >= 30){
             setTextResult.setTextColor(Color.parseColor("#ff2626"))
             setTextResult.setText("Obese")
+            indicator.animate().rotation(165f).start()
+            indicator.pivotX = 150f
         }
-
-
-    }
-
-    private fun resetBMI(){
-
-        setTextResult.setText("")
-        setTextResult.setText("")
-        heightInput.setText("")
-        weightInput.setText("")
+        setBmiResult.setText(String.format("%.2f", result))
 
     }
 }
